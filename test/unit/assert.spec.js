f8cef5dca277752f1217f839a35c1185f6f1a476
irvlee <bonnie_irving@163.com>
2019-08-09 20:41:08 +0800

    add karma unittest

---
+++ test/unit/assert.spec.js
@@ -0,0 +1,15 @@
+import getTruthy from '../../src/index'
+
+describe('unit test falsy', function(){
+    it('truthy', function(){
+        var result = getTruthy();
+        expect(result).toBeTruthy();     
+    });
+});                 
+        
+describe('unit test falsy', function(){
+    it('falsy', function(){
+        var result = !getTruthy();
+        expect(result).toBeFalsy();     
+    });
+});
